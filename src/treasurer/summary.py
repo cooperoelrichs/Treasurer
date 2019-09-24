b0063da4e4d7433fc071128834bea4b3ba1544a4
c.oelrichs <c.oelrichs@gmail.com>
2019-09-24 17:44:44 +0200

    Updates to the summary formulae.

--- src/treasurer/summary.py
+++ src/treasurer/summary.py
@@ -1,11 +1,14 @@
 import os
 import re
+import textwrap
 
 import numpy as np
 import pandas as pd
 
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 from matplotlib import rc
+
 
 from palettable.colorbrewer.diverging import Spectral_10 as clrs
 from palettable.colorbrewer.sequential import Blues_9 as blues
@@ -14,6 +17,7 @@ from palettable.colorbrewer.sequential import Reds_9 as reds
 
 class SummaryTools():
     FY_OFFSET = -6
+    LAST_FY_OF_THE_BALI_ACC = 2017
 
     def fy_series(df):
         return pd.DatetimeIndex(df['date']
@@ -33,10 +37,29 @@ class SummaryTools():
     def fy_summary(df):
         by_year = df.groupby(SummaryTools.fy_series(df))
         summary = by_year.sum()
-        summary['Balance'] = by_year.apply(
+
+        SummaryTools.calculated_balance(summary)
+        summary['Balance'] = SummaryTools.calculated_balance(summary)
+
+        post_bali_filter = summary.index > SummaryTools.LAST_FY_OF_THE_BALI_ACC
+        summary.loc[post_bali_filter, 'Balance'] = by_year.apply(
             lambda ser: ser.iloc[-1,]
-        )['Balance']
+        ).loc[post_bali_filter, 'Balance']
         return summary
+
+    def calculated_balance(summary):
+        years = summary.index.values
+        balances = []
+        prev_balance = 0
+        for i, y in enumerate(years):
+            balances += [
+                prev_balance +
+                summary['Income'][summary.index == y].sum() +
+                summary['Expenses'][summary.index == y].sum()
+            ]
+            prev_balance = balances[-1]
+
+        return balances
 
     def project_summary(df):
         return df.groupby(df['Project Codes']).sum()
@@ -46,8 +69,9 @@ class SummaryTools():
 
     def make_formatters():
         return {
-            'Expenses': SummaryTools.dollar_formatter,
             'Income': SummaryTools.dollar_formatter,
+            'Expenses': SummaryTools.dollar_formatter,
+            'Balance': SummaryTools.dollar_formatter,
         }
 
     def make_groups_filter_fn(df, groups):
@@ -74,9 +98,14 @@ class SummaryTools():
         return summaries
 
 
-    def pie_chart(df, field):
+    def pie_chart(df, field, img_dir, arrows):
+        fig_size = 4
+        v_margin = -0.1
+        h_margin = 1.8
+
         labels = [
-            ('%.1f%% - ' % (df.loc[a, field] / df.sum() * 100) + re.sub(r'-', ' ', a))
+            # '%.1f%% - ' % (df.loc[a, field] / df.sum() * 100) + re.sub(r'-', ' ', a))
+            textwrap.fill(re.sub(r'-', ' ', a), width=25)
             for a in df.index
         ]
 
@@ -88,21 +117,38 @@ class SummaryTools():
         else:
             raise ValueError(field)
 
-        fig = plt.figure(figsize=(10, 6), constrained_layout=False)
-        ax = fig.add_axes([0,0,1,1])
+        fig = plt.figure(
+            facecolor=None, edgecolor=None, frameon=True,
+            figsize=(fig_size, fig_size),
+            constrained_layout=False,
+            dpi=200
+        )
+
         plt.subplots_adjust(0,0,1,1)
+
         colours = cmap.hex_colors
-        img, texts = plt.pie(
+        _, texts = plt.pie(
             df.values[:, 0],
             labels=labels,
-            center=(0, 0),
+            # center=(0, 0),
             colors=colours,
-            wedgeprops={'linewidth': 1, 'edgecolor': 'white'}
+            wedgeprops={'linewidth': 0.8, 'edgecolor': 'black'}
         )
         [t.set_fontsize(12) for t in texts]
 
-        # if field == 'Expenses':
-        #     plt.arrow(1.08, 0.03, -0.04, 0.0, head_width=0.025, head_length=0.02)
+        plt.gcf().gca().add_artist(plt.Circle(
+            (0,0), 0.8, facecolor='lightgrey', edgecolor='black',
+            linewidth=0.8)
+        )
 
-        p=plt.gcf()
-        p.gca().add_artist(plt.Circle((0,0), 0.6, color='white'))
+        if field in arrows:
+            x, y, dx, dy = arrows[field]
+            plt.arrow(x, y, dx, dy, head_width=0.03, head_length=0.03)
+
+        bbox = mpl.transforms.Bbox([
+            [-h_margin, -v_margin], [fig_size+h_margin, fig_size+v_margin]
+        ])
+        plt.savefig(
+            fname=os.path.join(img_dir, '{}.png'.format(field)),
+            bbox_inches=bbox,
+        )
